624ef14faa6ce672acb12cbed2a1994493872a56
Stefy <stefytomasich@gmail.com>
2023-11-17 12:57:00 -0300

    GitIgnore Updates
    Saque muchas cosas a ver si explota todo (?)

--- Assets/Scripts/Movement.cs
+++ Assets/Scripts/Movement.cs
@@ -13,12 +13,7 @@ public class Movement : MonoBehaviour
 
     public Vector2 MoveDirection;
 
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
-
+    
     // Update is called once per frame
     void Update()
     {
